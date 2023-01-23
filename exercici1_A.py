cb4526f08c1453175590fd95966ba420175aa2b7
Iván <imontero22@jaumebalmes.net>
2023-01-23 19:54:55 +0100

    Estos son los ejercicios de ivan

---
+++ exercici1_A.py
@@ -0,0 +1,4 @@
+#Esta función calcula tu edad , en el parametro naixement se debe introducir el año de nacimiento.
+def anys(naixement):
+    return 2023-naixement
+print(anys(1997))
---
+++ exercici2_A.py
@@ -0,0 +1,8 @@
+#Este es un diccionario creado de 6 items , cada uno con su key y su value
+miDiccionario={'puente':15,'plaza':18,'farola':31,'edificio':15,'quiosco':30,'badulaque':31}
+#Esta función te de la longitud del diccionario
+print(len(miDiccionario))
+#La funcion values te devuelve solo los valores de las keys
+print(miDiccionario.values())
+#La función popitem te devuelve el último item del diccionario
+print(miDiccionario.popitem())
